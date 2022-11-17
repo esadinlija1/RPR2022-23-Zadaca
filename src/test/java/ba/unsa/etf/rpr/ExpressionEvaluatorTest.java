4563a57b1755cb0138d0b88e5ca300d76f3f0022
esadinlija1 <92315289+esadinlija1@users.noreply.github.com>
2022-11-17 21:56:34 +0100

    Initial commit

---
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class ExpressionEvaluatorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+   // @Test
+   // public void shouldAnswerWithTrue()
+    {
+        //assertTrue( true );
+    }
+}
