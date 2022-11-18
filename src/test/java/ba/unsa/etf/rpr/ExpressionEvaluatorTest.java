68418c8f4963edc68086bff13c28da125c15f0ef
esadinlija1 <92315289+esadinlija1@users.noreply.github.com>
2022-11-18 21:29:17 +0100

    Added javadoc comments for methods in test class

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -13,30 +13,45 @@ public class ExpressionEvaluatorTest
 {
     private static ExpressionEvaluator evaluator=new ExpressionEvaluator();
 
+    /**
+     * Test for expression which multiplies two numbers
+     */
     @Test
     public void getTwoNumbersProduct(){
         String s="( 3 * 6 )";
         assertEquals(18.0,evaluator.evaluate(s));
     }
 
+    /**
+     * Basic sqrt test
+     */
     @Test
     public void getSqrt(){
         String s="( sqrt 16 )";
         assertEquals(4.0,evaluator.evaluate(s));
     }
 
+    /**
+     * sqrt and another operator test
+     */
     @Test
     public void getSqrt2(){
         String s="( 3 + sqrt ( 4 ) )";
         assertEquals(5.0,evaluator.evaluate(s));
     }
 
+    /**
+     * Multiple brackets in expression test
+     */
     @Test
     public void multiOperatorsResult(){
         String s="( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
         assertEquals(101.0,evaluator.evaluate(s));
     }
 
+    /**
+     * Two expressions written differently but give same result
+     */
     @Test
     public void compareTwoExpressions(){
         String s="( 1 + ( 5 * ( 4 * 5 ) ) )";
@@ -44,16 +59,26 @@ public class ExpressionEvaluatorTest
         assertEquals(val,evaluator.evaluate(s));
     }
 
+    /**
+     * Exception test when user misunderstood completely how input works
+      */
     @Test
     public void basicExceptionTest(){
         assertThrows(RuntimeException.class, ()->{evaluator.evaluate("(3*4)");});
     }
 
+    /**
+     * Test for exception when exception is probably jus mistake in single place
+     */
     @Test
     public void exceptionTest2(){
         assertThrows(RuntimeException.class, ()->{evaluator.evaluate("( 1 + (5 * 4 ) ");});
     }
 
+
+    /**
+     * Test method to check if double sqrt operation works
+     */
     @Test
     public void bitMoreComplexTest(){
         assertEquals(2.0,evaluator.evaluate("( sqrt ( sqrt 16 ) )"));
